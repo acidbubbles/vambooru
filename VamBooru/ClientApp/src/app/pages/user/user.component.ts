26540f7b7656e105d8ba66a61965417f5cd0ab20
Acid Bubbles <acidbubbles.anon@gmail.com>
2018-04-26 14:09:31 -0400

    Show user's posts

--- VamBooru/ClientApp/src/app/pages/user/user.component.ts
+++ VamBooru/ClientApp/src/app/pages/user/user.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Subscription } from "rxjs/Subscription";
 import { ActivatedRoute } from "@angular/router";
-import { IUser } from "../../model/user";
 import { UsersService } from "../../services/users-service";
+import { PostsService, PostSortBy, PostSortDirection, PostedSince, IPostQuery } from "../../services/posts-service";
+import { IUser } from "../../model/user";
+import { IPost } from "../../model/post";
 
 @Component({
   selector: "user",
@@ -10,15 +12,40 @@ import { UsersService } from "../../services/users-service";
 })
 export class UserComponent implements OnInit, OnDestroy {
 	user: IUser;
+	posts: IPost[];
+	postsError:string ;
 	routeSub: Subscription;
 
-	constructor(private readonly route: ActivatedRoute, private readonly usersService: UsersService) { }
+	constructor(private readonly route: ActivatedRoute, private readonly usersService: UsersService, private readonly postsService: PostsService) { }
 
 	ngOnInit() {
 		this.routeSub = this.route.params.subscribe(params => {
 			this.user = null;
+			this.posts = null;
+			this.postsError = null;
 			this.usersService.getUser(params["id"]).subscribe(result => {
 				this.user = result;
+
+				var query: IPostQuery = {
+					sort: PostSortBy.votes,
+					direction: PostSortDirection.down,
+					since: PostedSince.forever,
+					page: 0,
+					pageSize: 100,
+					tags: [],
+					author: this.user.username,
+					text: ""
+				};
+				this.postsService
+					.searchPosts(query)
+					.subscribe(
+						posts => {
+							this.posts = posts;
+						},
+						error => {
+							this.postsError = error.message;
+						}
+					);
 			});
 		});
 	}
